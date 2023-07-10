f95503244c42cd15cfe7dd0fa74f8505baf938d5
huangjie4 <1364045672@qq.com>
2023-07-10 22:15:09 +0800

    修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,20 @@
 import { createRouter,createWebHashHistory} from 'vue-router'
 import Index from '@/pages/index.vue'
+import About from '@/pages/about.vue'
+import NotFound from '@/pages/404.vue'
 const routes = [
     {
         path:'/',
         component:Index
+    },
+    {
+        path:'/about',
+        component:About
+    },
+    {
+        path:'/:pathMatch(.*)*',
+        component:NotFound,
+        name:'NotFound'
     }
 ]
 const router = createRouter({
