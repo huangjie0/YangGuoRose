ff289dc11609022d4236e0feb7ac5a5899fe85f1
huangjie4 <1364045672@qq.com>
2023-07-10 22:04:12 +0800

    修改

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import router from './router'
+const app = createApp(App)
+app.use(ElementPlus)
+app.use(router)
+app.mount('#app')
