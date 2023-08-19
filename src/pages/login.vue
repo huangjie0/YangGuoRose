9eadb4dc1ab52a295099f05d4f91b24c45587e82
huangjie4 <1364045672@qq.com>
2023-08-19 16:20:08 +0800

    修改

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,13 +1,29 @@
 <template>
     <el-row class="rose-bg-p rose-vh">
-        <el-col :span="16" class="rose-f-c left rose-f-column">
+        <el-col :lg="16" :md="12" class="rose-f-c left rose-f-column">
             <div>
                 <div class="hello rose-font-w2">欢迎光临</div>
                 <div class="title">洋果子后台系统</div>
             </div>
         </el-col>
-        <el-col :span="8" class="rose-bg-w rose-f-c rose-f-column">
-            右边
+        <el-col :lg="8" :md="12" class="rose-bg-w rose-f-c rose-f-column">
+            <h2 class="rose-font-w2 rose-font-s1">欢迎回来</h2>
+            <div class="rose-f1 login-title">
+                <span class="line"></span>
+                <span>账号密码登录</span>
+                <span class="line"></span>
+            </div>
+            <el-form :model="form">
+                <el-form-item>
+                    <el-input v-model="form.userName" placeholder="请输入用户名"/>
+                </el-form-item>
+                <el-form-item>
+                    <el-input v-model="form.passWord" placeholder="请输入密码"/>
+                </el-form-item>
+                <el-form-item>
+                    <el-button round type="primary" color="#a781ee" @click="onSubmit" class="btn-w">登录</el-button>
+                </el-form-item>
+            </el-form>
         </el-col>
     </el-row>
 </template>
@@ -16,14 +32,8 @@ import { reactive } from 'vue'
 
 // do not use same name with ref
 const form = reactive({
-    name: '',
-    region: '',
-    date1: '',
-    date2: '',
-    delivery: false,
-    type: [],
-    resource: '',
-    desc: '',
+    userName: '',
+    passWord: '',
 })
 
 const onSubmit = () => {
@@ -32,6 +42,23 @@ const onSubmit = () => {
 
 </script>
 <style scoped lang="less">
+.el-button{
+    color: var(--rose-w);
+}
+.btn-w{
+    width: 250px;
+}
+.login-title{
+    margin: 25px 0;
+    color: var(--rose-g1);
+    font-size: 15px;
+    .line{
+        height: 1px;
+        width: 50px;
+        background-color: var(--rose-g1);
+        margin: 0 5px;
+    }
+}
 .left {
     color: var(--rose-w);
 
