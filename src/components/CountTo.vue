2c251e32c53cc40eab5355c37a047f9cdfda8e8f
huangjie4 <1364045672@qq.com>
2023-11-09 15:47:59 +0800

    修改

---
+++ src/components/CountTo.vue
@@ -0,0 +1,21 @@
+<template>
+    {{ d.num }}
+</template>
+<script setup>
+import { reactive } from 'vue'
+import gsap from 'gsap'
+
+defineProps({
+    value:
+})
+
+const d = reactive({
+    num:0
+}) 
+
+
+</script>
+<style lang="less">
+    
+
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,49 +1,73 @@
 <template>
-    <div>
-        <el-row :gutter="20">
-            <el-col :span="6" v-for="(item,index) in panels" :key="index">
-                <el-card  shadow="always">
+  <div>
+    <el-row :gutter="20">
+        <template v-if="panels.length == 0">
+            <el-col :span="6" v-for="i in 4" :key="i">
+                <el-skeleton animated loading>
+                <template #template>
+                    <el-card shadow="always">
                     <template #header>
                         <div class="rose-f1">
-                            <span class="rose-font-s2">{{ item.title }}</span>
-                            <el-tag :type="item.unitColor" effect="plain">
-                                {{ item.unit }}
-                            </el-tag>
+                            <el-skeleton-item variant="text" style="width: 50%"/>
+                            <el-skeleton-item variant="text" style="width: 10%"/>
                         </div>
                     </template>
                     <span class="rose-font-s1 rose-font-w2 count-value">
-                        {{ item.value }}
+                        <el-skeleton-item variant="h3"  style="width: 80%"/>
                     </span>
                     <el-divider />
                     <div class="rose-f1 rose-font-s3 rose-bg">
-                        <span>{{ item.subTitle }}</span>
-                        <span>{{ item.subValue }} </span>
+                        <el-skeleton-item variant="text" style="width: 50%"/>
+                        <el-skeleton-item variant="text" style="width: 10%"/>
                     </div>
-                </el-card>
+                    </el-card>
+                </template>
+                </el-skeleton>
             </el-col>
-        </el-row>
-    </div>
+        </template>
+
+        <el-col :span="6" v-for="(item, index) in panels" :key="index">
+            <el-card shadow="always">
+            <template #header>
+                <div class="rose-f1">
+                <span class="rose-font-s2">{{ item.title }}</span>
+                <el-tag :type="item.unitColor" effect="plain">
+                    {{ item.unit }}
+                </el-tag>
+                </div>
+            </template>
+            <span class="rose-font-s1 rose-font-w2 count-value">
+                {{ item.value }}
+            </span>
+            <el-divider />
+            <div class="rose-f1 rose-font-s3 rose-bg">
+                <span>{{ item.subTitle }}</span>
+                <span>{{ item.subValue }} </span>
+            </div>
+            </el-card>
+        </el-col>
+    </el-row>
+  </div>
 </template>
 <script setup>
-import { getStatistics1 } from '@/api/index.js'
-import { onMounted , ref } from 'vue'
+import { getStatistics1 } from "@/api/index.js";
+import { onMounted, ref } from "vue";
 
-const panels = ref([])
-     
+const panels = ref([]);
+
 const getDate = () => {
-    getStatistics1().then((res) => {
-        panels.value = res.panels
-        console.log(panels.value);
-    })
-}
+  getStatistics1().then((res) => {
+    panels.value = res.panels;
+  });
+};
 
 onMounted(() => {
-    getDate()
-})
-                                                     
+  getDate();
+});
+
 </script>
 <style lang="less" scoped>
-.count-value{
-    color: var(--rose-g4);
+.count-value {
+  color: var(--rose-g4);
 }
-</style>    
+</style>
