0e031b72fd57b91ad143987710107bbe78336a99
huangjie4 <1364045672@qq.com>
2024-01-02 14:24:33 +0800

    修改

--- src/pages/coupon/List.vue
+++ src/pages/coupon/List.vue
@@ -1,11 +1,87 @@
 <template>
-    <div>
-        优惠券
-    </div>
-</template>
-<script setup>
-
-</script>
-<style lang="less">
-    
-</style>
+    <el-card shadow="always">
+      <ListHeader @create="handleCreate" @refresh="getData"/>
+      <el-table :data="tableData" stripe style="width: 100%" v-loading="loading" :height="tableHeight">
+        <el-table-column prop="title" label="公告标题" width="380" />
+        <el-table-column prop="create_time" label="发布时间" width="380" />
+        <el-table-column label="操作" with="380" align="center">
+          <template #default="scope"> 
+            <el-button size="small" @click="handleEdit(scope.row)">修改</el-button>
+            <el-popconfirm title="是否要删除改公告？" confirm-button-text="确认" cancel-button-text="取消" @confirm="handleDelete(scope.row.id)">
+                <template #reference>
+                    <el-button size="small" type="danger" >删除</el-button>
+                </template>
+            </el-popconfirm>
+          </template>
+        </el-table-column>
+      </el-table>
+      <div class="rose-f-row pagination">
+        <el-pagination
+          background
+          layout="prev,pager,next"
+          :total="total"
+          :current-page="currentPage"
+          :page-size="limit"
+          @current-change="getData"
+        />
+      </div>
+      <FormDrawer ref="formDrawerRef" :title="drawerTitle" @submit="handleSubmit"> 
+          <el-form :model="form" ref="formRef" :rules="rules" label-width="80px" :inline="false">
+            <el-form-item label="公告标题" prop="title"> 
+              <el-input v-model="form.title" placeholder="公告标题"></el-input>
+            </el-form-item>
+            <el-form-item label="公告内容" prop="content"> 
+              <el-input v-model="form.content" placeholder="公告内容" type="textarea" :rows="5"></el-input>
+            </el-form-item>
+          </el-form>
+      </FormDrawer>
+    </el-card>
+  </template>
+  <script setup>
+  import { computed } from 'vue';
+  import { getCouponList,createCoupon,updateCoupon,deleteCoupon,updateCouponStatus } from '@/api/coupon.js'
+  import FormDrawer from '@/components/FormDrawer.vue';
+  import { useInitTable,useInitForm } from '@/composables/useCommon.js'
+  import ListHeader from '@/components/ListHeader.vue'
+  
+  const { tableData,loading,currentPage,total,limit,getData,handleDelete} = useInitTable({
+    getList:getNoticeList,
+    delete:deleteNotice,
+  })
+  
+  const { formDrawerRef,formRef,form,drawerTitle,rules,handleSubmit,handleCreate,handleEdit } = useInitForm({
+    getData,
+    update:updateNotice,
+    create:createNotice,
+    form:{
+      title:'',
+      content:''
+    },
+    rules:{
+      title:[{
+          required: true,
+          message: '公告标题不能为空！',
+          trigger: 'blur'
+      }],
+    content:[{
+          required: true,
+          message: '公告内容不能为空！',
+          trigger: 'blur'
+      }]
+    }
+  })
+  
+  const tableHeight = computed(()=>{
+    return (window.innerHeight - 270) + 'px';
+  }
+  )
+  
+  </script>
+  <style lang="less">
+  .pagination{
+    justify-content: center;
+    align-items: center;
+    margin-top:20px;
+  }
+  </style>
+  
