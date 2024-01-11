6189a67c0fd7c3ef61004d8d2b281d351cc50b22
huangjie4 <1364045672@qq.com>
2024-01-11 15:41:16 +0800

    xg

--- src/pages/goods/List.vue
+++ src/pages/goods/List.vue
@@ -86,28 +86,47 @@
         />
       </div>
       <FormDrawer ref="formDrawerRef" :title="drawerTitle" @submit="handleSubmit"> 
-          <el-form :model="form" ref="formRef" :rules="rules" label-width="80px" :inline="false">
-            <el-form-item label="用户名" prop="usename"> 
-              <el-input v-model="form.username" placeholder="请输入用户名"></el-input>
+          <el-form :model="form" ref="formRef" :rules="rules" label-width="85px" :inline="false">
+            <el-form-item label="商品名称" prop="title"> 
+              <el-input v-model="form.title" placeholder="请输入商品名称,不能超过60个字符"></el-input>
             </el-form-item>
-            <el-form-item label="密码" prop="password"> 
-              <el-input v-model="form.password" placeholder="请输入密码"></el-input>
+            <el-form-item label="封面" prop="cover"> 
+              <ChooseImage v-model="form.cover"></ChooseImage>
             </el-form-item>
-            <el-form-item label="头像" prop="avatar"> 
-              <ChooseImage v-model="form.avatar"></ChooseImage>
-            </el-form-item>
-            <el-form-item label="所属角色" prop="role_id"> 
-              <el-select v-model="form.role_id" placeholder="选择所属角色">
-                <el-option
-                  v-for="item in roles"
-                  :key="item.id"
-                  :label="item.name"
-                  :value="item.id"
-                />
+            <el-form-item label="商品分类" prop="category_id"> 
+              <el-select v-model="form.category_id" placeholder="请选择商品分类" size="small" clearable>
+                  <el-option v-for="item in category_list" :key="item.id" :label="item.name" :value="item.id"/>
               </el-select>
             </el-form-item>
-            <el-form-item label="状态" prop="status"> 
-              <el-switch  active-color="#a781ee" v-model="form.status" :active-value="1" :inactive-value="0"/>
+            <el-form-item label="商品描述" prop="desc"> 
+              <el-input v-model="form.desc" placeholder="请输入商品卖点" type="textarea"></el-input>
+            </el-form-item>
+            <el-form-item label="单位" prop="unit"> 
+              <el-input v-model="form.unit" placeholder="请输入单位"></el-input>
+            </el-form-item>
+            <el-form-item label="总库存" prop="stock"> 
+              <el-input-number v-model="form.stock" :min="0" :step="1" step-strictly></el-input-number>
+            </el-form-item>
+            <el-form-item label="库存预警" prop="min_stock"> 
+              <el-input-number v-model="form.min_stock" :min="0" :step="1" step-strictly></el-input-number>
+            </el-form-item>
+            <el-form-item label="最低销售价" prop="min_price"> 
+              <el-input-number v-model="form.min_price" :min="0" :step="1" step-strictly></el-input-number>
+            </el-form-item>
+            <el-form-item label="最低原价" prop="min_oprice"> 
+              <el-input-number v-model="form.min_oprice" :min="0" :step="1" step-strictly></el-input-number>
+            </el-form-item>
+            <el-form-item label="库存显示" prop="stock_display"> 
+              <el-radio-group v-model="form.stock_display">
+                <el-radio label="0">隐藏</el-radio>
+                <el-radio label="1">显示</el-radio>
+              </el-radio-group>
+            </el-form-item>
+            <el-form-item label="是否上架" prop="status"> 
+              <el-radio-group v-model="form.status">
+                <el-radio label="0">放入仓库</el-radio>
+                <el-radio label="1">立即上架</el-radio>
+              </el-radio-group>
             </el-form-item>
           </el-form>
       </FormDrawer>
@@ -125,7 +144,6 @@
   import Search from '@/components/Search.vue';
   import SearchItem from '@/components/SearchItem.vue'
   
-  const roles = ref([])
   const { searchForm,reset,tableData,loading,currentPage,total,limit,getData,handleDelete,handleChange } = useInitTable({
     getList:getGoodsList,
     delete:deleteGoods,
@@ -136,7 +154,6 @@
         return o
       })
       total.value = res.totalCount
-      roles.value = res.roles
     },
     searchForm:{
       title:'',
@@ -150,11 +167,17 @@ const { formDrawerRef,formRef,form,rules,drawerTitle,handleSubmit,handleCreate,h
   update:updateGoods,
   create:createGoods,
   form:{
-    username: "",
-    password: "",
-    role_id: null,
-    status: 1,
-    avatar: "",
+    title:"",	 		
+    category_id:null,
+    cover:null,
+    desc:"", 
+    unit:"件",
+    stock:100,
+    min_stock:0,
+    status:1,
+    stock_display:1,
+    min_price:0,
+    min_oprice:0,
   }
 })
 
