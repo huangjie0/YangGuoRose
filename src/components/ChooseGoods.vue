9e5d58ed56a38e29d635962b4ffd57149333c914
huangjie4 <1364045672@qq.com>
2024-03-19 10:00:07 +0800

    修改

--- src/components/ChooseGoods.vue
+++ src/components/ChooseGoods.vue
@@ -1,12 +1,21 @@
 <template>
-    <CommonDialog title="商品选择">
+    <CommonDialog title="商品选择" width="80%" ref="commonDialogRef">
         <template #content>
             111
         </template>
     </CommonDialog>
 </template>
 <script setup>
+import { ref } from "vue";
 import CommonDialog from './CommonDialog.vue';
+
+const commonDialogRef = ref(null)
+
+
+defineExpose({
+    commonDialogRef
+})
+
 </script>
 <style lang="less">
 
