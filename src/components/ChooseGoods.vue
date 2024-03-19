c9427e2e773a2a958dc7ff616289c627fcf27dbb
huangjie4 <1364045672@qq.com>
2024-03-19 09:55:14 +0800

    修改

---
+++ src/components/ChooseGoods.vue
@@ -0,0 +1,13 @@
+<template>
+    <CommonDialog title="商品选择">
+        <template #content>
+            111
+        </template>
+    </CommonDialog>
+</template>
+<script setup>
+import CommonDialog from './CommonDialog.vue';
+</script>
+<style lang="less">
+
+</style>
