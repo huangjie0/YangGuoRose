92db2de66f6cc6fd4c6bd5e64e977eb6248c13e2
huangjie4 <1364045672@qq.com>
2024-03-20 09:39:32 +0800

    修改

---
+++ src/api/level.js
@@ -0,0 +1,29 @@
+import axios from "@/axios.js";
+
+export function getUserLevelList(page){
+    return axios.get(`/admin/user_level/${page}`)
+}
+
+export function createRole(data){
+    return axios.post(`/admin/role`,data)
+}
+
+export function updateRole(id,data){
+    return axios.post(`/admin/role/${id}`,data)
+}
+
+export function deleteRole(id){
+    return axios.post(`/admin/role/${id}/delete`)
+}
+
+export function setRoleRules(id,rule_ids){
+    return axios.post(`/admin/role/set_rules`,{
+        id,rule_ids
+    })
+}
+
+export function updateRoleStatus(id,status){
+    return axios.post(`/admin/role/${id}/update_status`,{
+        status
+    })
+}
