6b8ec68dd6814d25e0ce10e1475cb2c0ffc60f8a
huangjie4 <1364045672@qq.com>
2024-03-21 10:05:43 +0800

    修改

--- src/pages/level/List.vue
+++ src/pages/level/List.vue
@@ -2,18 +2,18 @@
     <el-card shadow="always">
       <ListHeader @create="handleCreate" @refresh="getData"/>
       <el-table :data="tableData" stripe style="width: 100%" v-loading="loading" :height="tableHeight">
-        <el-table-column prop="name" label="角色名称" width="380" />
-        <el-table-column prop="desc" label="角色描述" width="380" />
-        <el-table-column label="状态" align="center" width="120">
+        <el-table-column prop="name" label="会员等级"/>
+        <el-table-column prop="discount" label="折扣率" align="center" />
+        <el-table-column prop="level" label="等级序号" align="center" />
+        <el-table-column label="状态" align="center">
             <template #default="scope">
               <el-switch :disabled = "scope.row.super == 1" :loading="scope.row.statusLoading" active-color="#a781ee" v-model="scope.row.status" :active-value="1" :inactive-value="0" @change="handleChange($event,scope.row)"/>
             </template>
         </el-table-column>
         <el-table-column label="操作" with="380" align="center">
           <template #default="scope"> 
-            
             <el-button size="small" @click="handleEdit(scope.row)">修改</el-button>
-            <el-popconfirm title="是否要删除改公告？" confirm-button-text="确认" cancel-button-text="取消" @confirm="handleDelete(scope.row.id)">
+            <el-popconfirm title="是否要删除改记录？" confirm-button-text="确认" cancel-button-text="取消" @confirm="handleDelete(scope.row.id)">
                 <template #reference>
                     <el-button size="small" type="danger" >删除</el-button>
                 </template>
@@ -33,40 +33,53 @@
       </div>
       <FormDrawer ref="formDrawerRef" :title="drawerTitle" @submit="handleSubmit"> 
           <el-form :model="form" ref="formRef" :rules="rules" label-width="80px" :inline="false">
-            <el-form-item label="角色名称" prop="name"> 
-              <el-input v-model="form.name" placeholder="请输入角色名称"></el-input>
+            <el-form-item label="等级名称" prop="name"> 
+                <el-input v-model="form.name" placeholder="请输入等级名称"></el-input>
             </el-form-item>
-            <el-form-item label="角色描述" prop="desc"> 
-              <el-input v-model="form.desc" placeholder="请输入角色描述" type="textarea" :rows="5"></el-input>
+            <el-form-item label="等级权重" prop="level"> 
+                <el-input v-model="form.level" placeholder="请输入等级权重" type="number"></el-input>
             </el-form-item>
-            <el-form-item label="状态" prop="status"> 
+            <el-form-item label="是否启用" prop="status"> 
                 <el-switch active-color="#a781ee" v-model="form.status" :active-value="1" :inactive-value="0"/>
             </el-form-item>
+            <el-form-item label="升级条件"> 
+                <div>
+                    <small>累计消费满</small>
+                    <el-input v-model="form.max_price" placeholder="请输入累计消费" type="number">
+                        <template #append>元</template>
+                    </el-input>
+                    <small>
+                        设置会员等级所需要的累计消费必须大于等级0，单位：元
+                    </small>
+                </div>
+                <div>
+                    <small>累计次数满</small>
+                    <el-input v-model="form.max_times" placeholder="请输入累计次数" type="number">
+                        <template #append>次</template>
+                    </el-input>
+                    <small>
+                        设置会员的等级所需要的购买量必须大于等于0，单位：笔
+                    </small>
+                </div>
+            </el-form-item>
+            <el-form-item label="折扣率" prop="discount"> 
+                <el-input v-model="form.discount" placeholder="请输入折扣率" type="number">
+                    <template #append>%</template>
+                </el-input>
+                <small>
+                    折扣率单位为百分比，如输入90，表示改会员等级的用户可以以商品原价的90%购买
+                </small>
+            </el-form-item>
           </el-form>
-      </FormDrawer>
-      <!-- 权限配置 -->
-      <FormDrawer ref="setRoleFormDrawerRef" title="权限配置" @submit="handleSetRoleSubmit"> 
-        <el-tree :check-strictly="checkStrictly" node-key="id" @check="handleTreeCheck" ref="elTreeRef" :default-expanded-keys="defaultExpandedKeys" :data="ruleList" :props="{ label:'name',children:'child' }" show-checkbox>
-          <template #default="{ node, data }">
-            <div class="rose-f-row roleTag">
-                <el-tag :type="data.menu ? '' : 'info'" size="small">
-                  {{ data.menu ? '菜单' : '权限' }}
-                </el-tag>
-                <span class="rose-ml-1 rose-font-s3">{{ data.name }}</span>
-            </div>
-          </template>
-        </el-tree>
       </FormDrawer>
     </el-card>
   </template>
   <script setup>
-  import { computed,ref } from 'vue';
+  import { computed } from 'vue';
   import { getUserLevelList,createUserLevel,updateUserLevel,deleteUserLevel,updateUserLevelStatus } from '@/api/level.js';
   import FormDrawer from '@/components/FormDrawer.vue';
   import { useInitTable,useInitForm } from '@/composables/useCommon.js';
   import ListHeader from '@/components/ListHeader.vue';
-  import { getRuleList } from '@/api/rules.js';
-  import { toast } from "@/composables/util.js";
   
   const { tableData,loading,currentPage,total,limit,getData,handleDelete,handleChange } = useInitTable({
     getList:getUserLevelList,
@@ -80,55 +93,30 @@
     create:createUserLevel,
     form:{
       name:'',
-      desc:'',
+      level:100,
       status:1,
+      discount:0,
+      max_price:0,
+      max_times:0
     },
     rules:{
       name:[{
           required: true,
-          message: '角色名称不能为空！',
+          message: '会员等级不能为空！',
           trigger: 'blur'
       }]
     }
   })
-
-  const setRoleFormDrawerRef = ref(null)
-  const elTreeRef = ref(null)
-  const ruleList = ref([])
-  const ruleId = ref(0)
-  const defaultExpandedKeys = ref([])
-  const ruleIds = ref([])
-  const checkStrictly = ref(false)
    
   const tableHeight = computed(()=>{
     return (window.innerHeight - 270) + 'px';
   }
   )
-
-  const handleSetRoleSubmit = ()=>{
-    setRoleFormDrawerRef.value.showLoading()
-    setRoleRules(ruleId.value,ruleIds.value).then(res=>{
-      toast('配置成功！')
-      getData()
-      setRoleFormDrawerRef.value.close()
-    }).finally(()=>{
-      setRoleFormDrawerRef.value.hideLoading()
-    })
-  }
-
-  const handleTreeCheck = (...e)=>{
-    const { checkedKeys , halfCheckedKeys } = e[1]
-    ruleIds.value = [ ...checkedKeys , ...halfCheckedKeys]
-  }
   
   </script>
   <style lang="less">
   .pagination{
     margin-top:20px;
-  }
-
-  .roleTag{
-    align-items: center;
   }
   </style>
   
