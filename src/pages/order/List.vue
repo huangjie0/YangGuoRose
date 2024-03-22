839df81402633e7992a78eb962a282bab1af9e94
huangjie4 <1364045672@qq.com>
2024-03-22 11:07:42 +0800

    修改

--- src/pages/order/List.vue
+++ src/pages/order/List.vue
@@ -7,8 +7,8 @@
       <el-card shadow="always">
         <!-- 表单公共搜索区域 -->
         <Search @search="getData" @reset="reset" :model="searchForm">
-            <SearchItem label="关键词">
-              <el-input v-model="searchForm.title" placeholder="商品名称" clearable></el-input>
+            <SearchItem label="订单编号">
+              <el-input v-model="searchForm.no" placeholder="订单编号" clearable></el-input>
             </SearchItem>
             <!-- <template #show>
               <SearchItem label="商品分类">
@@ -19,59 +19,63 @@
             </template> -->
         </Search>
   
-        <ListHeader @create="handleCreate" @refresh="getData" layout="create,refresh">
-          <el-button  size="small" @click="moreDelete" v-if="searchForm.tab !== 'delete'" type="danger">批量删除</el-button>
-          <el-button  size="small" @click="moreRecover" v-else type="warning">恢复商品</el-button>
-          <el-popconfirm title="是否要彻底删除改商品？" v-if="searchForm.tab == 'delete'" confirm-button-text="确认" cancel-button-text="取消" @confirm="cleanOut">
-              <template #reference>
-                <el-button size="small" type="danger">彻底删除</el-button>
-              </template>
-          </el-popconfirm>
-          <el-button  size="small" @click="moreUnmount(1)" v-show="searchForm.tab == 'all' || searchForm.tab == 'off'">上架</el-button>
-          <el-button  size="small" @click="moreUnmount(0)" v-show="searchForm.tab == 'all' || searchForm.tab == 'saling'">下架</el-button>
+        <ListHeader @create="handleCreate" @refresh="getData" layout="refresh">
+          <el-button  size="small" @click="moreDelete" type="danger">批量删除</el-button>
         </ListHeader>
   
         <el-table :data="tableData" stripe style="width: 100%" v-loading="loading" :height="tableHeight" @selection-change="handleSelectionChange" ref="tableRef">
           <el-table-column type="selection" width="55" />
-          <el-table-column label="商品" width="300">
+          <el-table-column label="商品">
               <template #default="scope">
                 <div class="rose-f-row">
-                  <div class="rose-f-c rose-mr-1">
-                    <el-image :src="scope.row.cover" fit="cover" :lazy="true" class="smallImage"></el-image>
-                  </div>
-                  <div class="rose-f-1">
-                    <p>{{ scope.row.title }}</p>
-                    <div>
-                      <span class="rose-font-red">￥{{ scope.row.min_price }}</span>
-                      <el-divider direction="vertical"></el-divider>
-                      <span class="rose-bg">￥{{ scope.row.min_oprice }}</span>
+                    <div class="rose-f-1">
+                        <p>订单号：</p>
+                        <small>{{ scope.row.no }}</small>
                     </div>
-                    <p>分类：{{ scope.row.category ? scope.row.category.name : "未分类" }}</p>
-                    <p>创建时间：{{ scope.row.create_time }}</p>
-                  </div>
+                    <div>
+                        <p>下单时间：</p>
+                        <small>{{ scope.row.create_time }}</small>
+                    </div>
+                </div>
+                <div class="rose-f-row" v-for="(item,index) in scope.row.order_items" :key="index">
+                    <el-image :src="item.goods_item ?.cover ?? ''" fit="fill" :lazy="true" class="smallImage"></el-image>
+                    <p>{{ item.goods_item ?.title ?? '商品已经被删除' }}</p>
                 </div>
               </template> 
           </el-table-column>
-          <el-table-column label="实际销量" width="80" prop="sale_count">
+          <el-table-column label="实际付款" width="80" prop="total_price">
           </el-table-column>
-          <el-table-column label="商品状态" width="100">
+          <el-table-column label="买家" align="center">
               <template #default="scope">
-                <el-tag :type="scope.row.status ? 'success' : 'danger'" size="small">{{ scope.row.status ? '上架' : '仓库' }}</el-tag>
+                <p>{{ scope.row.user.nickname || scope.row.user.username }}</p>
+                <small>(用户ID： {{ scope.row.user.id }})</small>
               </template>
           </el-table-column>
-          <el-table-column label="审核状态" width="200" v-if="searchForm.tab !== 'delete'">
+          <el-table-column label="交易状态" align="center">
               <template #default="scope">
-                <div class="rose-f-row" v-if="scope.row.ischeck == 0 ">
-                  <el-button type="success" size="small" plain>审核通过</el-button>
-                  <el-button type="danger" size="small" plain>审核拒绝</el-button>
+                <div>
+                    付款状态：
+                    <el-tag type="success" size="small" v-if="scope.row.payment_method == 'alipay'">微信支付</el-tag>
+                    <el-tag type="primary" size="small" v-else-if="scope.row.payment_method == 'wechat'">支付宝支付</el-tag>
+                    <el-tag type="info" size="small" v-else>未支付</el-tag>
                 </div>
-                <span v-else>{{ scope.row.ischeck == 1 ? '通过' : '拒绝' }}</span>
+                <div>
+                    发货状态：
+                    <el-tag :type="scope.row.ship_data ? 'success' :'info' " size="small" >{{ scope.row.ship_data ? '已发货' : '未发货' }}</el-tag>
+                </div>
+                <div>
+                    收货状态：
+                    <el-tag :type="scope.row.ship_status == 'received' ? 'success' :'info' " 
+                    size="small" >{{ scope.row.ship_status == 'received' ? '已收货' : '未收货' }}</el-tag>
+                </div>
               </template>
           </el-table-column>
-          <el-table-column label="总库存" width="80" prop="stock"></el-table-column>
-          <el-table-column label="操作" with="350" align="center">
+          <el-table-column label="操作" align="center">
             <template #default="scope"> 
-                <el-button size="small" @click="handleEdit(scope.row)">修改</el-button>
+                <el-button size="small" @click="handleEdit(scope.row)">订单详情</el-button>
+                <el-button size="small" v-show="searchForm.tab == 'noship'" @click="handleEdit(scope.row)">订单发货</el-button>
+                <el-button size="small" v-show="searchForm.tab == 'refunding'" @click="handleEdit(scope.row)">同意退款</el-button>
+                <el-button size="small" v-show="searchForm.tab == 'refunding'" @click="handleEdit(scope.row)">拒绝退款</el-button>
             </template>
           </el-table-column>
         </el-table>
@@ -93,12 +97,12 @@ import { ref, computed, onMounted } from 'vue';
 import { getOrderList, deleteOrder } from '@/api/order.js';
 import {useInitTable} from '@/composables/useCommon.js';
 import ListHeader from '@/components/ListHeader.vue';
-import { getCategoryList } from '@/api/category.js';
+// import { getCategoryList } from '@/api/category.js';
 import Search from '@/components/Search.vue';
 import SearchItem from '@/components/SearchItem.vue';
 
-const { searchForm,reset,tableData,loading,currentPage,total,limit,getData,handleDelete,handleSelectionChange,tableRef,
-moreDelete,moreUnmount,moreRecover,cleanOut} = useInitTable({
+const { searchForm,reset,tableData,loading,currentPage,total,limit,getData,handleSelectionChange,tableRef,
+moreDelete} = useInitTable({
 getList:getOrderList,
 delete:deleteOrder,
 onGetListSuccess:(res)=>{
@@ -111,9 +115,12 @@ onGetListSuccess:(res)=>{
     total.value = res.totalCount
 },
 searchForm:{
-    title:'',
+    no:'',
     tab:'all',
-    category_id:null
+    starttime:'',
+    endtime:'',
+    name:'',
+    phone:''
 }
 })
 
@@ -124,21 +131,23 @@ const tableHeight = computed(()=>{
 
 const tabbars = [
     {key:'all',name:'全部'},
-    {key:'checking',name:'审核中'},
-    {key:'saling',name:'出售中'},
-    {key:'off',name:'已下架'},
-    {key:'min_stock',name:'库存预警'},
-    {key:'delete',name:'回收站'}
+    {key:'nopay',name:'待支付'},
+    {key:'noship',name:'待发货'},
+    {key:'shiped',name:'待收货'},
+    {key:'received',name:'已收货'},
+    {key:'finish',name:'已完成'},
+    {key:'closed',name:'已关闭'},
+    {key:'refunding',name:'退款中'}
 ]
 
 // 商品分类
-const category_list = ref([])
+// const category_list = ref([])
 
-onMounted(() => {
-getCategoryList().then(res=>{
-    category_list.value = res
-})
-})
+// onMounted(() => {
+// getCategoryList().then(res=>{
+//     category_list.value = res
+// })
+// })
 
 </script>
 <style lang="less" scoped>
