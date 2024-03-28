90205a52a56912a0a8cc3a45307aead4e011c51f
huangjie4 <1364045672@qq.com>
2024-03-28 13:43:42 +0800

    修改

--- src/pages/setting/Buy.vue
+++ src/pages/setting/Buy.vue
@@ -17,7 +17,7 @@
                         </el-table-column>
                         <el-table-column label="操作" align="center" width="">
                             <template #default="{row}">
-                                <el-button  size="small" type="primary">配置</el-button>
+                                <el-button  size="small" type="primary" @click="disposition(row)">配置</el-button>
                             </template>
                         </el-table-column>
                     </el-table>
@@ -47,14 +47,57 @@
                 </el-tab-pane>
             </el-tabs>
         </el-form>
+
+        <FormDrawer title="配置" ref="formDrawerRef" @submit="submitDisposition">
+            <el-form :model="form" label-width="120" v-if="drawerType == 'alipay'">
+                <el-form-item label="app_id">
+                    <el-input v-model="form.alipay.app_id" placeholder="app_id" type="textarea" rows="4"></el-input>
+                </el-form-item>
+                <el-form-item label="ali_public_key">
+                    <el-input v-model="form.alipay.ali_public_key" placeholder="ali_public_key" type="textarea" rows="4"></el-input>
+                </el-form-item>
+                <el-form-item label="private_key">
+                    <el-input v-model="form.alipay.private_key" placeholder="private_key" type="textarea" rows="4"></el-input>
+                </el-form-item>
+            </el-form>
+            <el-form :model="form" label-width="120" v-else>
+                <el-form-item label="app_id">
+                    <el-input v-model="form.wxpay.app_id" placeholder="app_id" type="textarea" rows="4"></el-input>
+                </el-form-item>
+                <el-form-item label="miniapp_id">
+                    <el-input v-model="form.wxpay.miniapp_id" placeholder="miniapp_id" type="textarea" rows="4"></el-input>
+                </el-form-item>
+                <el-form-item label="secret">
+                    <el-input v-model="form.wxpay.secret" placeholder="secret" type="textarea" rows="4"></el-input>
+                </el-form-item>
+                <el-form-item label="appid">
+                    <el-input v-model="form.wxpay.appid" placeholder="appid" type="textarea" rows="4"></el-input>
+                </el-form-item>
+                <el-form-item label="mch_id">
+                    <el-input v-model="form.wxpay.mch_id" placeholder="mch_id" type="textarea" rows="4"></el-input>
+                </el-form-item>
+                <el-form-item label="key">
+                    <el-input v-model="form.wxpay.key" placeholder="key" type="textarea" rows="4"></el-input>
+                </el-form-item>
+                <el-form-item label="cert_client">
+                    <el-input v-model="form.wxpay.cert_client" placeholder="cert_client" type="textarea" rows="4"></el-input>
+                </el-form-item>
+                <el-form-item label="cert_key">
+                    <el-input v-model="form.wxpay.cert_key" placeholder="cert_key" type="textarea" rows="4"></el-input>
+                </el-form-item>
+            </el-form>
+        </FormDrawer>
     </div>
 </template>
 <script setup>
 import { toast } from '@/composables/util.js';
 import { ref, reactive } from 'vue';
 import { getSysconfig,setSysconfig } from '@/api/sysconfig.js';
+import FormDrawer from '@/components/FormDrawer.vue';
 
 const loading = ref(false);
+const formDrawerRef = ref(null);
+const drawerType = ref("alipay");
 const getData = ()=>{
     loading.value = true
     getSysconfig().then(res=>{
@@ -65,6 +108,16 @@ const getData = ()=>{
     }).finally(()=>{
         loading.value = false
     })
+}
+
+const disposition = (row)=>{
+    drawerType.value = row.key
+    formDrawerRef.value.open()
+}
+
+const submitDisposition = ()=>{
+    
+
 }
 
 const activeName = ref("first");
@@ -84,24 +137,24 @@ const tableData = [
 ]
 
 const form = reactive({
-    "close_order_minute": 30,
-    "auto_received_day": 7,
-    "after_sale_day": 23,
-    "alipay": {
-        "app_id": "...",
-        "ali_public_key": "...",
-        "private_key": "..."
+    close_order_minute: 30,
+    auto_received_day: 7,
+    after_sale_day: 23,
+    alipay: {
+        app_id: "...",
+        ali_public_key: "...",
+        private_key: "..."
     },
-    "wxpay": {
-        "app_id": "...",
-        "miniapp_id": "...",
-        "secret": "...",
-        "appid": "...",
-        "mch_id": "...",
-        "key": "...",
-        "cert_client": "....pem",
-        "cert_key": "....pem"
-    },
+    wxpay: {
+        app_id: "...",
+        miniapp_id: "...",
+        secret: "...",
+        appid: "...",
+        mch_id: "...",
+        key: "...",
+        cert_client: "....pem",
+        cert_key: "....pem"
+    }
 })
 
 const submit = ()=>{
